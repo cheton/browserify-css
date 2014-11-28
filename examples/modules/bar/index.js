dd3557def32ccce28227354677c57599625b7b4d
Cheton Wu (RD-TW) <cheton_wu@trend.com.tw>
2014-11-27 23:49:21 -0800

    Update examples

---
+++ examples/modules/bar/index.js
@@ -0,0 +1,9 @@
+module.exports = {
+    init: function() {
+        var container = document.getElementById('container');
+        var bar = document.createElement('div');
+        bar.setAttribute('class', 'bar');
+        bar.innerHTML = 'bar module';
+        container.appendChild(bar);
+    }
+};
