7e1b9bb88910c9a16371838dd669f2338d9a5ba5
Cheton Wu <cheton@gmail.com>
2015-02-19 20:05:30 +0800

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+'use strict';
+
+var gulp = require('gulp');
+var jshint = require('gulp-jshint');
+
+var tasks = {
+    lintjs: function() {
+        var jshintConfig = require('./config/jshint');
+        return gulp.src([
+            '*.js',
+            '*.json',
+            'examples/**/*.js',
+            '!examples/bundle.js'
+        ])
+        .pipe(jshint(jshintConfig))
+        .pipe(jshint.reporter());
+    }
+};
+
+gulp.task('lint:js', tasks.lintjs);
+
+gulp.task('default', [
+    'lint:js'
+]);
