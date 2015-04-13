718a3df0fb94e58ff2fc82695e816d2d5930b1c4
Cheton Wu (RD-TW) <cheton_wu@trend.com.tw>
2015-04-12 23:49:51 -0700

    Add 'processRelativeUrl' option to customize relative URL in the CSS file (#13).

--- gulp/config.js
+++ gulp/config.js
@@ -43,31 +43,29 @@ module.exports = {
                     },
                     'rootDir': 'examples/submodules',
                     'minify': true,
-                    'processRelativeUrl': function(url) {
+                    'processRelativeUrl': function(relativeUrl) {
                         var stripQueryStringAndHashFromPath = function(url) {
                             return url.split('?')[0].split('#')[0];
                         };
                         var rootDir = path.resolve(process.cwd(), 'examples/submodules');
-                        var relativePath = stripQueryStringAndHashFromPath(url);
-                        var queryStringAndHash = url.substring(relativePath.length);
+                        var relativePath = stripQueryStringAndHashFromPath(relativeUrl);
+                        var queryStringAndHash = relativeUrl.substring(relativePath.length);
 
                         //
                         // Copying files from '../node_modules/bootstrap/' to 'vendor/bootstrap/'
                         //
-                        (function() {
-                            var prefix = '../node_modules/';
-                            if (_.startsWith(relativePath, prefix)) {
-                                var newUrl = 'vendor/' + relativePath.substring(prefix.length);
-                                var source = path.join(rootDir, relativePath);
-                                var target = path.join(rootDir, newUrl);
+                        var prefix = '../node_modules/';
+                        if (_.startsWith(relativePath, prefix)) {
+                            var newUrl = 'vendor/' + relativePath.substring(prefix.length);
+                            var source = path.join(rootDir, relativePath);
+                            var target = path.join(rootDir, newUrl);
 
-                                gutil.log('Copying file from ' + JSON.stringify(source) + ' to ' + JSON.stringify(target));
-                                fse.copySync(source, target);
+                            gutil.log('Copying file from ' + JSON.stringify(source) + ' to ' + JSON.stringify(target));
+                            fse.copySync(source, target);
 
-                                // Returns a new url with original query string and hash fragments
-                                return newUrl + queryStringAndHash;
-                            }
-                        }());
+                            // Returns a new url with original query string and hash fragments
+                            return newUrl + queryStringAndHash;
+                        }
 
                         return url;
                     }
